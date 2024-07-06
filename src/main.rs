d387c0440ddd13510b3617a9024e41e99185c72b
LAPTOP-PHKF3UCR\user <tjsm@moais.co.kr>
2024-07-06 22:51:34 +0900

    [feature] 시스템에 카메라 모듈 추가. 및 연동
    - 기본 카메라 초기화 함수 추가
    - Startup 시스템에 카메라 생성 함수 연결

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
+use ts1_game_2048_rust::system::camera;
 
 fn main() {
     App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, camera::spawn_camera)
     .run();
 }
