5007686cabd537e090df75a2ee8c0ce1e7499943
LAPTOP-PHKF3UCR\user <tjsm@moais.co.kr>
2024-07-06 23:21:30 +0900

    [feature] 전체적인 프로그램 제어를 위해 기본적인 키보드 핸들 연결

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
-use ts1_game_2048_rust::system::camera;
+use ts1_game_2048_rust::system;
 
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
-    .add_systems(Startup, camera::spawn_camera)
+    .add_systems(Startup,
+         system::camera::spawn_camera)
+    .add_systems(Update, 
+        system::handle_keyboard_input::handle_keyboard_input)
     .run();
 }
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,1 +1,2 @@
 pub mod camera;
+pub mod handle_keyboard_input;
