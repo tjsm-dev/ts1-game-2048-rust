91a281be64c0a557fbb5dcd89b90f714c277a26d
shsh26 <sagong126@gmail.com>
2024-07-07 20:25:04 +0900

    [feature] 타일 정의

---
+++ src/component/tile.rs
@@ -0,0 +1,28 @@
+use bevy::prelude::*;
+
+use crate::entity::TileState;
+
+
+#[derive(Component)]
+pub struct Tile {
+    pub value: u32,
+    pub position_x: i32,
+    pub position_y: i32,
+    pub state: TileState,
+}
+
+impl Tile {
+    pub fn new(value: u32, position_x: i32, position_y: i32) -> Self {
+        Tile {
+            value,
+            position_x,
+            position_y,
+            state: TileState::Spawned,
+        }
+    }
+
+    pub fn merge(&mut self, value: u32) {
+        self.value += value;
+        self.state = TileState::Merged;
+    }
+}
