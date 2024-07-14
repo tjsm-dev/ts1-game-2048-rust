b25e9d88ac35e29640231955fedbe5990b123304
shsh26 <sagong126@gmail.com>
2024-07-14 14:34:57 +0900

    [feature] 빈 위치 자료구조 추가 및 타일 생성 함수 수정

--- src/component/board.rs
+++ src/component/board.rs
@@ -1,5 +1,6 @@
+use std::collections::HashSet;
 use bevy::prelude::*;
-use rand::random;
+use rand::{thread_rng, Rng};
 
 use crate::component::tile::Tile;
 use crate::entity::defines::{TILE_HEIGHT, TILE_WIDTH};
@@ -9,41 +10,62 @@ use crate::entity::defines::{TILE_HEIGHT, TILE_WIDTH};
 pub struct Board {
     pub tiles: Vec<Tile>,
     pub score: u16,
+    empty_positions: HashSet<(u8, u8)>,
 }
 
 impl Board {
     pub fn new() -> Self {
+        let mut empty_positions = HashSet::new();
+        for x in 0..TILE_WIDTH {
+            for y in 0..TILE_HEIGHT {
+                empty_positions.insert((x, y));
+            }
+        }
         let mut board = Board {
             tiles: Vec::<Tile>::new(),
             score: 0,
+            empty_positions,
         };
         board.spawn_tile();
         board.spawn_tile();
         board
     }
 
-    pub fn spawn_tile(&mut self) {
-        if self.tiles.len() >= (TILE_WIDTH * TILE_HEIGHT) as usize {
-            return;
-        }
-        // TODO: Implement a better way to spawn tiles
-        loop {
-            let x = (random::<u32>() % TILE_WIDTH as u32) as u8;
-            let y = (random::<u32>() % TILE_HEIGHT as u32) as u8;
-
-            if self.get_tile(x, y).is_none() {
-                let score = if random::<u32>() % 10 == 0 {
-                    4
-                } else {
-                    2
-                };
-                self.tiles.push(Tile::new(score, x, y));
-                break;
-            }
-        }
+    pub fn add_tile(&mut self, tile: Tile) {
+        self.empty_positions.remove(&(tile.position_x, tile.position_y));
+        self.tiles.push(tile);
     }
 
-    fn get_tile(&self, x: u8, y: u8) -> Option<&Tile> {
-        self.tiles.iter().find(|tile| tile.position_x == x && tile.position_y == y)
+    pub fn spawn_tile(&mut self) -> Option<&Tile> {
+        if self.empty_positions.is_empty() {
+            return None;
+        }
+
+        let mut rng = thread_rng();
+        let empty_positions: Vec<&(u8, u8)> = self.empty_positions.iter().collect();
+        let &(x, y) = empty_positions[rng.gen_range(0..empty_positions.len())];
+
+        // 60% chance of spawning a 2, 40% chance of spawning a 4
+        let value = if rng.gen_bool(0.6) { 2 } else { 4 };
+
+        let tile = Tile::new(value, x, y);
+        self.add_tile(tile);
+        self.tiles.last()
+    }
+
+    pub fn get_tile(&self, x: u8, y: u8) -> Option<&Tile> {
+        self.tiles.iter().find(|t| t.position_x == x && t.position_y == y)
+    }
+
+    pub fn get_tiles(&self) -> &Vec<Tile> {
+        &self.tiles
+    }
+
+    pub fn count_tiles(&self) -> usize {
+        self.tiles.len()
+    }
+
+    pub fn get_empty_positions(&self) -> &HashSet<(u8, u8)> {
+        &self.empty_positions
     }
 }
