cb5ed294ffde1fa38b2e423355989f6d8523515e
tjsm-dev <tjsm@moais.co.kr>
2024-09-11 21:41:30 +0900

    게임 윈도우 색상 변경, 타이틀 세팅, 크기 결정

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,31 @@ use ts1_game_2048_rust::system::resource::GameContext;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.88, 0.88, 0.88)))
         .insert_resource(Boards::default())
         .insert_resource(GameContext::default())
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            // 기본 해상도 설정
+            primary_window: Some(Window {
+                title: "2047!".to_string(),
+                resolution: (1000.0, 1000.0).into(),
+                ..default()
+            }),
+            ..Default::default()
+        }))
         .add_event::<system::events::ChangeGameStatus>()
         .add_event::<system::events::MoveTiles>()
         .add_systems(Startup, (system::camera::spawn_camera, system::game::spawn_board))
         .add_systems(Update,
             (
                 system::handle_keyboard_input::handle_keyboard_input,
+                system::window_util::handle_window_close,
+            ),
+        )
+        .add_systems(Update,
+            (
                 ui::show_menu::show_menu.after(
                     system::handle_keyboard_input::handle_keyboard_input),
-                system::window_util::handle_window_close,
             ),
         )
         .run();
