0b7ebb1c0f5a065a8269973119462eabf53b5236
shsh26 <sagong126@gmail.com>
2024-10-23 21:22:45 +0900

    TileState 삭제

--- src/component/tile.rs
+++ src/component/tile.rs
@@ -1,24 +1,19 @@
 use bevy::prelude::*;
 
-use crate::entity::defines::TileState;
-
 
 #[derive(Component, Debug, Clone, Copy, PartialEq)]
 pub struct Tile {
     pub value: u16,
-    pub state: TileState,
 }
 
 impl Tile {
     pub fn new(value: u16) -> Self {
         Tile {
             value,
-            state: TileState::Spawned,
         }
     }
 
     pub fn merge(&mut self, value: u16) {
         self.value += value;
-        self.state = TileState::Merged;
     }
 }
--- src/entity/defines.rs
+++ src/entity/defines.rs
@@ -2,10 +2,3 @@ use bevy::prelude::*;
 
 pub const TILE_WIDTH: usize = 4;
 pub const TILE_HEIGHT: usize = 4;
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum TileState {
-    Spawned,
-    Merged,
-    Moved,
-}
